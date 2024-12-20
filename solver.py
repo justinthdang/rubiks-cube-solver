5836e9424f79f92dc3d64a200bf8fa9ebe61bdfa
Justin Dang <tinhuydang@outlook.com>
2024-12-20 01:43:06 -0500

    put detected colours into list then convert into string for kociemba to solve

--- solver.py
+++ solver.py
@@ -13,51 +13,79 @@ colour_ranges = {
     "orange" : ([9, 160, 206],  [19, 255, 255]),
 }
 
+# convert colour to corresponding face
+def colour_to_face(colour):
+    if colour == "white":
+        return "U"
+    elif colour == "yellow":
+        return "D"
+    elif colour == "green":
+        return "F"
+    elif colour == "blue":
+        return "B"
+    elif colour == "red":
+        return "R"
+    elif colour == "orange":
+        return "L"
+
 # returns the corresopnding colour that falls between the colour ranges
 def detect_colour(hsv_value):
     for colour, (lower, upper) in colour_ranges.items():
         if all(lower[i] <= hsv_value[i] <= upper[i] for i in range(3)):
             return colour
 
-# draws grid and detects colours in each square
+# draws 9 squares from top-left to bottom-right, creating a 3x3 grid
 def grid(frame, colour, thickness):
     x_og, y_og = 362, 234
-
-    # loops to draw each square
+    face_colours = []
     for y in range(3):
         x_start, y_start = x_og, y_og + y * 100
         for x in range(3):
             x_end, y_end = x_start + 100, y_start + 100
             cv2.rectangle(frame, (x_start, y_start), (x_end, y_end), colour, thickness)
-
-            # detects the colour in the roi
+# detects the colour in the roi and adds to list
             roi = frame[y_start:y_end, x_start:x_end]
             hsv_roi = cv2.cvtColor(roi, cv2.COLOR_BGR2HSV)
             avg_hsv = np.mean(hsv_roi, axis=(0, 1)).astype(int)
             detected_colour = detect_colour(avg_hsv)
-
-            # places text of colour in grid
+            face_colours.append(detected_colour)
+# places text of colour in grid
             text_x, text_y = x_start + 10, y_start + 30
             cv2.putText(frame, detected_colour, (text_x, text_y), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 255, 255), 2)
-
             x_start += 100
-        
-cap = cv2.VideoCapture(0)
+    return face_colours
 
+# turn on webcam
+cap = cv2.VideoCapture(0)
+faces = []
+
+# webcam running
 while cap.isOpened():
     ret, frame = cap.read()
     frame = cv2.resize(frame, (1024, 768))
-
-    grid(frame, (255, 255, 255), 5)
+    key = cv2.waitKey(1) & 0XFF
+# calls function to detect colours of each sticker
+    current_face = grid(frame, (255, 255, 255), 5)
     cv2.imshow('Detect Faces', frame)
-
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+# "f" captures current face and adds it to list, "q" is quit
+    if key == ord("f"):
+        faces.append(current_face)
+    elif key == ord("q"):
         break
 
+# turn off webcam
 cap.release()
 cv2.destroyAllWindows()
 
+# convert list to string in face representation and solves cube
+converted = ""
+for face in faces:
+    for colour in face:
+        converted += colour_to_face(colour)
+kc.solve(converted)
+
 '''
+    kociemba library follows this order when capturing faces
     U = white
     R = red
     F = green
