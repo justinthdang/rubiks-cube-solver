42624d1ce1d60aa5bac309dc2e0bc40aa52d9ad2
Justin Dang <tinhuydang@outlook.com>
2024-12-21 01:27:36 -0500

    comment hsv_picker code and remove unnecessary functions

--- hsv_picker.py
+++ hsv_picker.py
@@ -1,40 +1,43 @@
 import numpy as np
 import cv2
 
+# turn on webcam
 cap=cv2.VideoCapture(0)
-
 def nothing(x):
     pass
+
+# create trackbars for lower and upper hsv values
 cv2.namedWindow("Trackbars")
-cv2.createTrackbar("L - H", "Trackbars", 0, 179, nothing)
-cv2.createTrackbar("L - S", "Trackbars", 0, 255, nothing)
-cv2.createTrackbar("L - V", "Trackbars", 0, 255, nothing)
-cv2.createTrackbar("U - H", "Trackbars", 179, 179, nothing)
-cv2.createTrackbar("U - S", "Trackbars", 255, 255, nothing)
-cv2.createTrackbar("U - V", "Trackbars", 255, 255, nothing)
+cv2.createTrackbar("Lower H", "Trackbars", 0, 179, nothing)
+cv2.createTrackbar("Lower S", "Trackbars", 0, 255, nothing)
+cv2.createTrackbar("Lower V", "Trackbars", 0, 255, nothing)
+cv2.createTrackbar("Upper H", "Trackbars", 179, 179, nothing)
+cv2.createTrackbar("Upper S", "Trackbars", 255, 255, nothing)
+cv2.createTrackbar("Upper V", "Trackbars", 255, 255, nothing)
 
+# webcam running
 while True:
-    ret,frame=cap.read()
-    frame=cv2.resize(frame,(1024,768))
+    ret,frame = cap.read()
+    frame = cv2.resize(frame, (1024, 768))
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-    
-    l_h = cv2.getTrackbarPos("L - H", "Trackbars")
-    l_s = cv2.getTrackbarPos("L - S", "Trackbars")
-    l_v = cv2.getTrackbarPos("L - V", "Trackbars")
-    u_h = cv2.getTrackbarPos("U - H", "Trackbars")
-    u_s = cv2.getTrackbarPos("U - S", "Trackbars")
-    u_v = cv2.getTrackbarPos("U - V", "Trackbars")
-    lower_blue = np.array([l_h, l_s, l_v])
-    upper_blue = np.array([u_h, u_s, u_v])
-    mask = cv2.inRange(hsv, lower_blue, upper_blue)
-    result = cv2.bitwise_and(frame, frame, mask=mask)    
-
-    # show thresholded image
-    cv2.imshow("mask", mask)
-    cv2.imshow("result", result)  
-
-    key = cv2.waitKey(1) & 0xFF
-    if key == ord("q"):
+# get trackbar positions
+    lower_h = cv2.getTrackbarPos("Lower H", "Trackbars")
+    lower_s = cv2.getTrackbarPos("Lower S", "Trackbars")
+    lower_v = cv2.getTrackbarPos("Lower V", "Trackbars")
+    upper_h = cv2.getTrackbarPos("Upper H", "Trackbars")
+    upper_s = cv2.getTrackbarPos("Upper S", "Trackbars")
+    upper_v = cv2.getTrackbarPos("Upper V", "Trackbars")
+# group lower and upper hsv values into arrays
+    lower = np.array([lower_h, lower_s, lower_v])
+    upper = np.array([upper_h, upper_s, upper_v])
+# create mask for hsv values that fall within specified range
+    mask = cv2.inRange(hsv, lower, upper)
+    result = cv2.bitwise_and(frame, frame, mask = mask)    
+    cv2.imshow("result", result)
+# "q" ends program
+    if cv2.waitKey(1) & 0xFF == ord("q"):
         break
+
+# turn off webcam
 cap.release()
 cv2.destroyAllWindows()
